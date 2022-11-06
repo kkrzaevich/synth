77b2f702b3af367a73161075a5f84464318d58b3
kkrzaevich <61949567+kkrzaevich@users.noreply.github.com>
2022-11-06 22:42:01 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,21 @@
+// Запускаем синтезатор
 
-// Создать аудио контекст
-const audioCtx = new AudioContext();
-
-// Создать гейн
-let gain = audioCtx.createGain();
-console.log(gain);
-console.log(gain.gain.value);
-
-// Создать осциллятор, задать тип
-const oscillator = audioCtx.createOscillator();
-oscillator.frequency.setValueAtTime(220, 0);
-oscillator.type = 'square';
-
-// Задать подключение осциллятора к гейну
-oscillator.connect(gain);
-
-// Задать подключение гейна к выходу звуковой карты
-gain.connect(audioCtx.destination);
+const context = new AudioContext();
+let gain = context.createGain();
+gain.gain.value = 0.1;
+gain.connect(context.destination);
 
 // Задаем октавный сдвиг
 let transposition = -1;
 
 // Создать класс ноты
 class Note {
-    constructor(name, octave, frequency) {
+    constructor(name, octave, frequency, oscillator) {
       this.name = name;
       this.octave = octave;
       this.frequency = frequency;
+      this.oscillator = oscillator;
     }
-}
-
-// Запускаем синтезатор
-function launchSynth() {
-    oscillator.start(0);
-    gain.gain.value = 0;
-    $('.launchButton').off();
-}
-
-// Синтезатор играет
-function playKey() {
-    oscillator.frequency.setValueAtTime(this.note.frequency, 0);
-    console.log(this.note.frequency + ' ' + this.note.name + ' ' + this.note.octave);
-    gain.gain.value = 1;
-}
-
-// Запуск с клавиатуры
-function playKeyboard(event) {
-    console.log(event.key.toUpperCase());
-    let keyPlayed = $(`button:contains('${event.key.toUpperCase()}')`)[0];
-    console.log(keyPlayed);
-    oscillator.frequency.setValueAtTime(keyPlayed.note.frequency, 0);
-    gain.gain.value = 1;
-}
-
-// Синтезатор перестал играть
-function stopKey() {
-    gain.gain.value = 0;
-    console.log('HUI');
 }
 
 // Задание частот
@@ -80,7 +39,7 @@ function setFrequencies() {
             else (console.log('Note out of range WTF???;'));
             console.log(currentNote);
             let button = document.querySelector(`.octave-${octave}`).children[i-1];
-            button.id = `${currentNote.name}${currentNote.octave}`;
+            button.id = 'key' + button.innerText;
             button.note = currentNote;
         }
     }
@@ -88,8 +47,50 @@ function setFrequencies() {
 
 // Действия вне функций
 setFrequencies();
-$('.launchButton').on('click', launchSynth);
-$('.key').on('mousedown', playKey);
-$('.key').on('mouseup', stopKey);
-$('.key').on('keydown', playKeyboard);
-$('.key').on('keyup', stopKey);
+
+// При игре клавиатурой
+
+$('.key').on('keydown', function(event) {
+    // Проверяем, что такая клавиша есть (НЕ РАБОТАЕТ, ПОФИКСИТЬ)
+    if (typeof document.querySelector(`#key${event.key.toUpperCase()}`) !== 'undefined') {
+        // Получаем частоту сыгранной ноты
+        currentFrequency = document.querySelector(`#key${event.key.toUpperCase()}`).note.frequency;
+        // Проверяем, что частота есть, и отсекаем репиты эвента от долго нажатых клавиш
+        if (currentFrequency > 0 && event.originalEvent.repeat === false) {
+            // Проверяем, что осциллятора на данной ноте еще нет (отсекает баги), создаем осциллятор
+            if (typeof document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator === 'undefined') {
+                document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator = context.createOscillator();
+                document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator.connect(gain);
+                document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator.start(0);
+                document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator.frequency.setValueAtTime(currentFrequency, 0);
+                document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator.type = 'sine';
+                console.log('adding ' + document.querySelector(`#key${event.key.toUpperCase()}`).note.frequency);
+                this.addEventListener('keyup', function(event) {
+                    if (typeof document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator !== 'undefined') {
+                        console.log('deleting ' + document.querySelector(`#key${event.key.toUpperCase()}`).note.frequency);
+                        document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator.stop(0);
+                        delete document.querySelector(`#key${event.key.toUpperCase()}`).note.oscillator;
+                    }
+                });
+            }
+        } 
+    } else { console.log('Wrong key')};
+});
+
+// При игре мышкой
+
+$('.key').on('mousedown mouseover', function(event) {
+    if (event.buttons == 1 || event.buttons == 3) {
+        currentFrequency = this.note.frequency;
+        this.note.oscillator = context.createOscillator();
+        this.note.oscillator.connect(gain);
+        this.note.oscillator.start(0);
+        this.note.oscillator.frequency.setValueAtTime(currentFrequency, 0);
+        this.note.oscillator.type = 'sine';
+        console.log(event);
+        $('.key').on('mouseup mouseout', function (event) { 
+            this.note.oscillator.stop(0);
+            delete this.note.oscillator;
+        });
+    }
+})
